a0754f8a712ba4b835846933e2fbcd3cb2ebe96c
Derek C <derek.canoponce20@gmail.com>
2026-09-22 21:55:36 -0700

    Create MyName.cpp

---
+++ MyName.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Program by Derek Cano Ponce" << endl;
+    cout << endl;
+
+    cout << "######   #####  ###### " << endl;
+    cout << "#     # #     # #     #" << endl;
+    cout << "#     # #       #     #" << endl;
+    cout << "#     # #       ###### " << endl;
+    cout << "#     # #       #      " << endl;
+    cout << "#     # #     # #      " << endl;
+    cout << "######   #####  #      " << endl;
+    cout << endl;
+
+    cout << "Date: Sept 11, 2026" << endl;
+
+    return 0;
+}
